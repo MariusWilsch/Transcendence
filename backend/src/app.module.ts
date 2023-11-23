c5237b7ce891d28fd8ccd2f39a537a8ed67f505c
imad <imad.mimouni.123@gmail.com>
2023-11-23 23:18:21 +0100

    authentication using google account

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ChatModule } from './modules/chat/chat.module';
 import { GameModule } from './modules/game/game.module';
 import { UserModule } from './modules/user/user.module';
+import { AuthModule } from 'modules/auth/auth.module';
+import { AuthController } from 'modules/auth/auth.controller';
 
 @Module({
-  imports: [ChatModule, GameModule, UserModule],
-  controllers: [AppController],
-	providers: [AppService],
+  imports: [ChatModule, GameModule, UserModule, AuthModule],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthModule],
 })
 export class AppModule {}
