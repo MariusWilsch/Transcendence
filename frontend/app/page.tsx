9f5835df2717be5b56cde82df6537fe017fd244e
imad <imad.mimouni.123@gmail.com>
2023-12-05 00:58:28 +0100

    front end touches

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,12 +5,23 @@ import logo from "../public/42_Logo.svg";
 import Link from "next/link";
 import { useEffect } from "react";
 
-
 export default function Home() {
-
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-200">
-        <Link
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://images.pexels.com/photos/976873/pexels-photo-976873.jpeg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">ft_transcendence</h1>
+          <p className="mb-5">
+            You can play Pong and chat with others.
+          </p>
+          <Link
           href="http://localhost:3001/auth/42"
           className="bg-white hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
         >
@@ -22,6 +33,8 @@ export default function Home() {
           className="inline-block mr-2"
         />
         </Link>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -5,6 +5,14 @@ import backArrow from "../../public/backArrow.svg";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 
+export function Loading() {
+  return (
+    <div className="bg-[#DDE6ED] h-screen w-screen flex items-center justify-center">
+      <span className="loading loading-dots loading-lg"></span>
+    </div>
+  );
+}
+
 type User = {
   intraId: string;
   fullname: string;
@@ -13,13 +21,85 @@ type User = {
   Avatar: string;
   created_at: Date;
   updated_at: Date;
-}
+};
+
+type UserDescriptionCardProps = {
+  title: string;
+  content: string;
+};
+
+const UserDescriptionCard = ({ title, content }: UserDescriptionCardProps) => {
+  return (
+    <div className="flex-1 h-[100%] mx-[3%]">
+      <div className="text-white h-[20%] pt-4 text-center text-xl font-bold font-mono bg-[#526D82] rounded-md">
+        {title}
+      </div>
+      <div className="text-[#27374D] pt-[7%] text-start bg-[#9DB2BF] h-[60%] rounded-lg mt-2 break-words overflow-hidden p-3">
+        &nbsp;&nbsp;&nbsp;&nbsp;{content}
+      </div>
+    </div>
+  );
+};
+
+
+const UserDetailsCard = ({value} : { value: string }) => {
+  return (
+    <div className="flex items-center justify-center w-screen h-[7vh] mb-[5vh]">
+      <div
+        className="flex items-center justify-center w-1/4 p-4
+       bg-[#27374D] shadow-inner rounded-md"
+      >
+        <div className="text-white text-xl font-serif"> {value} </div>
+      </div>
+    </div>
+  );
+};
+
+
+type UserProfileImageProps = {
+  src: string;
+};
+
+const UserProfileImage = ({ src }: UserProfileImageProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center mb-[5vh]">
+      <Image
+        src={src}
+        alt="backArrow"
+        width={300}
+        height={300}
+        priority={true}
+        quality={100}
+        className="rounded-full border-2 border-black"
+        style={{ width: "30vh", height: "30vh" }}
+      />
+    </div>
+  );
+};
+
+
+const BackButton = () => {
+  return (
+    <div className="pt-[5vh] px-[5vw] hover:opacity-70">
+      <Link href="http://localhost:3000" className="">
+        <Image
+          src={backArrow}
+          alt="home page"
+          width={30}
+          priority={true}
+          quality={75}
+          className=""
+          style={{ width: "2.5vh", height: "4vh" }}
+        />
+      </Link>
+    </div>
+  );
+};
 
 
 export default function Profile() {
-
   const [userData, setUserData] = useState<User | null>(null);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkJwtCookie = async () => {
@@ -31,123 +111,52 @@ export default function Profile() {
           },
           credentials: "include",
         });
-        var data : User = await response.json();
+        var data: User = await response.json();
 
         setUserData(data);
+        setLoading(false);
+
 
         console.log("data : ", data);
       } catch (error) {
         console.error("Error during login:", error);
+        setLoading(false);
       }
     };
     checkJwtCookie();
   }, []);
 
+  if (loading) {
+    return <Loading />;
+  }
 
   const Login = userData?.login || "Login";
-
 
   const FullName = userData?.fullname || "Full Name";
 
   const Title = userData?.intraId || "Title";
   const Content = userData?.email || "Email";
 
-  const IntraPic = userData?.Avatar || "http://m.gettywallpapers.com/wp-content/uploads/2023/05/Cool-Anime-Profile-Picture.jpg";
+  // const IntraPic = userData?.Avatar || "http://m.gettywallpapers.com/wp-content/uploads/2023/05/Cool-Anime-Profile-Picture.jpg";
+  const IntraPic = "http://m.gettywallpapers.com/wp-content/uploads/2023/05/Cool-Anime-Profile-Picture.jpg";
+
 
   return (
-    <div
-      className="bg-[#DDE6ED] h-screen w-screen 
-     "
-    >
-      <div className="pt-[7%] px-[7%] hover:opacity-80">
-        <Link href="http://localhost:3000" className="">
-          <Image
-            src={backArrow}
-            alt="backArrow"
-            width={30}
-            priority={true}
-            quality={75}
-            className=""
-          />
-        </Link>
-      </div>
+    <div className="bg-[#DDE6ED] h-screen w-screen">
+      <BackButton />
+      <UserProfileImage src={IntraPic} />
 
-      <div
-        className="flex flex-col items-center justify-center
-        mb-[5%]"
-      >
-        <Image
-          src={IntraPic}
-          alt="backArrow"
-          width={200}
-          height={200}
-          priority={true}
-          quality={100}
-          className="rounded-full border-2 border-black"
-        />
-      </div>
+      <UserDetailsCard value={Login} />
+      <UserDetailsCard value={FullName} />
 
-      <div className="flex items-center justify-center w-screen h-[10%]">
-        <div
-          className="flex items-center justify-center w-1/6 p-4
-         bg-[#27374D] shadow-inner  rounded-md "
-        >
-          <div className="text-white text-xl  font-serif"> {Login} </div>
-        </div>
-      </div>
-
-      <div className="flex items-center justify-center w-screen h-[10%] mb-[5%]">
-        <div
-          className="flex items-center justify-center w-1/4 p-4
-         bg-[#27374D] shadow-inner rounded-md"
-        >
-          <div className="text-white text-xl  font-serif"> {FullName} </div>
-        </div>
-      </div>
-
-      <div className="flex justify-center h-[50%]">
-        <div className="flex justify-around w-[90%] h-[100%]">
-          <div className="flex-1 h-[100%] mx-[3%]">
-            <div
-              className="text-white  h-[10%] pt-[7%] text-center 
-            text-xl font-bold font-mono bg-[#526D82] rounded-md"
-            >
-              {" "}
-              {Title}{" "}
-            </div>
-            <div className="text-[#27374D] pt-[7%] text-start bg-[#9DB2BF] h-[50%] rounded-lg mt-2 break-words overflow-hidden p-3">
-              &nbsp;&nbsp;&nbsp;&nbsp;
-              {Content}{" "}
-            </div>
-          </div>
-          <div className="flex-1 h-[100%] ml-5vw mx-[3%]">
-            <div
-              className="text-white  h-[10%] pt-[7%] text-center 
-            text-xl font-bold font-mono bg-[#526D82] rounded-md"
-            >
-              {" "}
-              {Title}{" "}
-            </div>
-            <div className="text-[#27374D] pt-[7%] text-start bg-[#9DB2BF] h-[50%] rounded-lg mt-2 break-words overflow-hidden p-3">
-              {" "}
-              {Content}{" "}
-            </div>{" "}
-          </div>
-          <div className="flex-1 h-[100%] ml-5vw mx-[3%]">
-            <div
-              className="text-white  h-[10%] pt-[7%] text-center 
-            text-xl font-bold font-mono bg-[#526D82] rounded-md"
-            >
-              {" "}
-              {Title}{" "}
-            </div>
-            <div className="text-[#27374D] pt-[7%] text-start bg-[#9DB2BF] h-[50%] rounded-lg mt-2 break-words overflow-hidden p-3">
-              {" "}
-              {Content}{" "}
-            </div>{" "}
-          </div>
+      <div className="flex justify-center h-[25vh] w-screen">
+        <div className="flex justify-around w-[90vw] h-[100%]">
+          <UserDescriptionCard title={Title} content={Content} />
+          <UserDescriptionCard title={Title} content={Content} />
+          <UserDescriptionCard title={Title} content={Content} />
         </div>
       </div>
     </div>
   );
 }
+
