40cd55f95f96cdd8f3347b3c60df8b0ac389557e
Imad Mimouni <imimouni@e2r7p14.1337.ma>
2024-02-05 00:32:37 +0100

    error handling

--- frontend/app/gamelobby/page.tsx
+++ frontend/app/gamelobby/page.tsx
@@ -20,37 +20,35 @@ export default function Home() {
 	useEffect(() => {
 		if (!context.socket || !context.user) return;
 		context.socket?.on('privateMatch', (data: any) => {
-			// if (data.from.intraId !== context.user?.intraId)
-			// {
 			const msg = 'invitation from ' + data.from.login + ' for a game';
 			toast((t) => (
-				<>
-					<p> {msg} </p>
-					<span className="flex flex-row space-x-6">
-						<button
-							className="w-full flex items-center justify-center text-sm font-medium text-indigo-600  hover:text-indigo-500 "
-							onClick={() => {
-								console.log('accept handler');
-								toast.dismiss(t.id);
-								handleInvite(context.user?.intraId, isConnected, Invite.ACCEPTING);
-							}}
-						>
-							<FiCheckCircle size="30" className="text-green-300" />
-						</button>
-						<button
-							className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium"
-							onClick={() => {
-								toast.dismiss(t.id);
-								console.log('dismiss handler');
-							}}
-						>
-							<FiXCircle size="30" className="text-red-300" />
-						</button>
-					</span>
-				</>
+				<div className="flex flex-row rounded-lg bg-slate-500 bg-opacity-80">
+					<div className='w-[60%]'> {msg} </div>
+					<button
+						className="w-[20%] flex items-center justify-center text-sm font-medium text-indigo-600  hover:text-indigo-500 "
+						onClick={() => {
+							console.log('accept handler');
+							toast.dismiss(t.id);
+							if (context.user)
+							{
+								handleInvite(context.user.intraId, isConnected, Invite.ACCEPTING);
+							}
+						}}
+					>
+						<FiCheckCircle size="30" className="text-green-300" />
+					</button>
+					<button
+						className="w-[20%] border border-transparent rounded-none rounded-r-lg p-3 flex items-center justify-center text-sm font-medium"
+						onClick={() => {
+							toast.dismiss(t.id);
+							console.log('dismiss handler');
+						}}
+					>
+						<FiXCircle size="30" className="text-red-300" />
+					</button>
+				</div>
 			));
 			console.log('the private chat event has been occured');
-			// }
 		});
 		return () => {
 			toast.dismiss();
@@ -59,7 +57,7 @@ export default function Home() {
 	}, [context.user]);
 
 	return (
-		<div className='text-white'>
+		<div className="text-white">
 			<Cards />
 			<Toaster />
 		</div>
